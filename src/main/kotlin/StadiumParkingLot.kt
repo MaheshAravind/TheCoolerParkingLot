04dbccfc2459d02adca894decab875ac0fea69ef
MaheshAravindV <maheshv@sahaj.ai>
2023-02-14 23:58:37 +0530

    Use EndExclusiveInterval to store time intervals in StadiumParkingLot

--- src/main/kotlin/StadiumParkingLot.kt
+++ src/main/kotlin/StadiumParkingLot.kt
@@ -1,4 +1,3 @@
-import INTERVALS.*
 import VehicleClass.FOUR_WHEELER
 import VehicleClass.TWO_WHEELER
 import exceptions.InvalidDurationException
@@ -7,6 +6,12 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 
 class StadiumParkingLot(twoWheelerCount: Int = 0, fourWheelerCount: Int = 0) : ParkingLot() {
+    companion object {
+        val INTERVAL_ONE = EndExclusiveInterval(0, 4)
+        val INTERVAL_TWO = EndExclusiveInterval(4, 12)
+        val INTERVAL_THREE = EndExclusiveInterval(12)
+    }
+
     override val validVehicleClasses: Set<VehicleClass> = setOf(TWO_WHEELER, FOUR_WHEELER)
 
     override val freeSpots: FreeSpots = FreeSpots(TWO_WHEELER to twoWheelerCount, FOUR_WHEELER to fourWheelerCount)
@@ -23,35 +28,18 @@ class StadiumParkingLot(twoWheelerCount: Int = 0, fourWheelerCount: Int = 0) : P
     }
 
     private fun findFourWheelerCost(hourCount: Int): Int {
-        val interval = findInterval(hourCount)
-        return when (interval) {
-            INTERVAL_ONE -> 60
-            INTERVAL_TWO -> 60 + 120
-            INTERVAL_THREE -> 60 + 120 + (hourCount - 12) * 200
-        }
+        if (INTERVAL_ONE.contains(hourCount)) return 30
+        if (INTERVAL_TWO.contains(hourCount)) return 30 + 60
+        if (INTERVAL_THREE.contains(hourCount)) return 30 + 60 + 100 * (hourCount - 12)
+
+        throw InvalidDurationException()
     }
 
     private fun findTwoWheelerCost(hourCount: Int): Int {
-        val interval = findInterval(hourCount)
-        return when (interval) {
-            INTERVAL_ONE -> 30
-            INTERVAL_TWO -> 30 + 60
-            INTERVAL_THREE -> 30 + 60 + (hourCount - 12) * 100
-        }
-    }
+        if (INTERVAL_ONE.contains(hourCount)) return 60
+        if (INTERVAL_TWO.contains(hourCount)) return 60 + 120
+        if (INTERVAL_THREE.contains(hourCount)) return 60 + 120 + 200 * (hourCount - 12)
 
-    private fun findInterval(hourCount: Int): INTERVALS {
-        return INTERVALS.values().find { it.contains(hourCount) } ?: throw InvalidDurationException()
-    }
-}
-
-enum class INTERVALS(private val start: Int, val end: Int) {
-    INTERVAL_ONE(0, 4),
-    INTERVAL_TWO(4, 12),
-    INTERVAL_THREE(12, -1);
-
-    fun contains(value: Int): Boolean {
-        if (end == -1) return value > start //end being -1 means it is infinity
-        return value in start..end
+        throw InvalidDurationException()
     }
 }
