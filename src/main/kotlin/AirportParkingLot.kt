ce47aac980a7cc6399295531ac705323920b6e3a
MaheshAravindV <maheshv@sahaj.ai>
2023-02-15 00:11:57 +0530

    Improve readability, handle edge scenario
    - Use until to improve readability
    - Handle invalid duration

--- src/main/kotlin/AirportParkingLot.kt
+++ src/main/kotlin/AirportParkingLot.kt
@@ -1,5 +1,6 @@
 import VehicleClass.FOUR_WHEELER
 import VehicleClass.TWO_WHEELER
+import exceptions.InvalidDurationException
 import exceptions.InvalidVehicleTypeException
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -21,21 +22,21 @@ class AirportParkingLot(twoWheelerCount: Int = 0, fourWheelerCount: Int = 0) : P
     }
 
     private fun findFourWheelerCost(hourCount: Int): Int {
-        return if (hourCount < 12) 60
-        else if (hourCount < 24) 80
-        else {
-            val dayCount = TimeUnit.HOURS.toDays(hourCount.toLong()).toInt()
-            dayCount * 100
-        }
+        if (hourCount in 0 until 12) return 60
+        if (hourCount in 12 until 24) return 80
+        val dayCount = TimeUnit.HOURS.toDays(hourCount.toLong()).toInt()
+        if (dayCount > 0) return dayCount * 100
+
+        throw InvalidDurationException()
     }
 
     private fun findTwoWheelerCost(hourCount: Int): Int {
-        return if (hourCount == 0) 0
-        else if (hourCount < 8) 40
-        else if (hourCount < 24) 60
-        else {
-            val dayCount = TimeUnit.HOURS.toDays(hourCount.toLong()).toInt()
-            dayCount * 80
-        }
+        if (hourCount in 0 until 1) return 0
+        if (hourCount in 1 until 8) return 40
+        if (hourCount in 8 until 24) return 60
+        val dayCount = TimeUnit.HOURS.toDays(hourCount.toLong()).toInt()
+        if (dayCount > 0) return dayCount * 80
+
+        throw InvalidDurationException()
     }
 }
