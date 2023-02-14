e2f7149c3ae305f48884303f4126149b35a027d9
MaheshAravindV <maheshv@sahaj.ai>
2023-02-14 22:42:56 +0530

    Add ParkingTicket to generate a parking ticket

---
+++ src/main/kotlin/ParkingTicket.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+class ParkingTicket(
+    private val ticketNumber: Int,
+    private val spotNumber: Int,
+    private val entryDateTime: Date,
+    private val vehicleClass: VehicleClass
+) {
+    override fun toString(): String {
+        return """
+            Parking Ticket:
+                Ticket Number: $ticketNumber
+                Spot Number: $vehicleClass - $spotNumber
+                Entry Date-time: $entryDateTime
+        """.trimIndent()
+    }
+
+    fun getVehicleClass() = vehicleClass
+    fun getSpotNumber() = spotNumber
+    fun getEntryDateTime() = entryDateTime
+}
